34992bb9740cec606031878e9c9685d7047ee77c
marco-rozo <marcorozo99@gmail..com>
2022-12-01 20:19:51 -0300

    Add toastfy para notificação de erro de cadastro

--- src/pages/CategoryFormPage/index.tsx
+++ src/pages/CategoryFormPage/index.tsx
@@ -1,8 +1,10 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { ICategory } from "../../commons/interfaces";
+import Notify from "../../commons/notify";
 import { Button } from "../../components/Buttons";
-import { Container } from "../../components/Container";
 import { DangerAlert } from "../../components/DangerAlert";
 import { Input } from "../../components/Inputs";
 import CategoryService from "../../service/CategoryService";
@@ -69,63 +71,41 @@ export function CategoryFormPage() {
         if (responseError.response.data.validationErrors) {
           setErrors(responseError.response.data.validationErrors);
         }
+        Notify.error("Erro ao cadastrar categoria");
         setPendingApiCall(false);
         setApiError(true);
       });
   };
 
   return (
-    <div className="mt-8 w-full relative flex flex-col justify-center overflow-hidden items-center">
-      <div className="w-4/5 md:w-1/2">
-        <h1 className="text-3xl mb-5 font-bold text-center text-purple-700">
-          Cadastro de Categoria
-        </h1>
-        <Input
-          label="Nome"
-          classNameLabel="block text-sm font-semibold text-gray-800"
-          placeholder="Nome"
-          type="text"
-          className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-          onChange={onChange}
-          value={form.name}
-          name="name"
-        />
-        <Button
-          text="Salvar"
-          disabled={pendingApiCall}
-          className="mt-5 w-full mb-3 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-600 rounded-md hover:bg-purple-500 focus:outline-none focus:bg-purple-500"
-          onClick={onSubmit}
-          pendingApiCall={pendingApiCall}
-        ></Button>
-        {apiError && <DangerAlert text="Falha ao cadastrar a categoria." />}
+    <>
+      <div className="mt-8 w-full relative flex flex-col justify-center overflow-hidden items-center">
+        <div className="w-4/5 md:w-1/2">
+          <h1 className="text-3xl mb-5 font-bold text-center text-purple-700">
+            Cadastro de Categoria
+          </h1>
+          <Input
+            label="Nome"
+            classNameLabel="block text-sm font-semibold text-gray-800"
+            placeholder="Nome"
+            type="text"
+            className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            onChange={onChange}
+            value={form.name}
+            name="name"
+          />
+          <Button
+            text="Salvar"
+            disabled={pendingApiCall}
+            className="mt-5 w-full mb-3 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-600 rounded-md hover:bg-purple-500 focus:outline-none focus:bg-purple-500"
+            onClick={onSubmit}
+            pendingApiCall={pendingApiCall}
+          ></Button>
+          {apiError && <DangerAlert text="Falha ao cadastrar a categoria." />}
+        </div>
       </div>
-    </div>
-    // <div className="container">
-    //   <h1 className="text-center">Cadastro de Categoria</h1>
 
-    //   <div className="col-12 mb-3">
-    //     <Input
-    //       className="form-control"
-    //       name="name"
-    //       label="Nome"
-    //       placeholder="Informe o nome"
-    //       type="text"
-    //       value={form.name}
-    //       onChange={onChange}
-    //       hasError={errors.name ? true : false}
-    //       error={errors.name}
-    //     />
-    //   </div>
-    //   {apiError && <DangerAlert text="Falha ao cadastrar a categoria." />}
-    //   <div className="text-center">
-    //     <Button
-    //       className="block text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-    //       onClick={onSubmit}
-    //       disabled={pendingApiCall ? true : false}
-    //       pendingApiCall={pendingApiCall}
-    //       text="Salvar"
-    //     />
-    //   </div>
-    // </div>
+      <ToastContainer />
+    </>
   );
 }
