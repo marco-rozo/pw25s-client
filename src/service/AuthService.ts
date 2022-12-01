1f0c6db6be9260d82b09ebb34072d28bf22a2a1c
marco-rozo <marcorozo99@gmail..com>
2022-12-01 20:19:03 -0300

    Add validação do token no servidor

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,34 +1,45 @@
+import { useEffect } from "react";
 import { IUserLogin, IUserSignUp } from "../commons/interfaces";
 import { api } from "../lib/axios";
 
 const signup = (user: IUserSignUp) => {
-  debugger;
   return api.post("/users", user);
 };
 
 const login = (user: IUserLogin) => {
-  debugger;
   return api.post("/login", user);
 };
 
 const logout = () => {
-  localStorage.removeItem('token');
-}
+  localStorage.removeItem("token");
+};
 
 const isAuthenticated = () => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
-      api.defaults.headers.common['Authorization'] = `Bearer ${JSON.parse(token)}`;
+    api.defaults.headers.common["Authorization"] = `Bearer ${JSON.parse(
+      token
+    )}`;
+
+    validateToken().catch((err) => {
+      logout();
+      window.location.reload();
+    });
   }
-  
+
   return token ? true : false;
+};
+
+const validateToken = () => {
+  return api.get("/auth");
 };
 
 const AuthService = {
   signup,
   login,
   isAuthenticated,
-  logout
+  logout,
+  validateToken,
 };
 
 export default AuthService;
