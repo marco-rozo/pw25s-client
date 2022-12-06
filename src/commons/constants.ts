9a0e7255de61268c045354b3731096fdb108fcb7
marco-rozo <marcorozo99@gmail..com>
2022-12-06 00:27:26 -0300

    Criado constantes para indices de tipos de contas

---
+++ src/commons/constants.ts
@@ -0,0 +1,6 @@
+export const typesAccounts = [
+  { id: "0", name: "Conta Universitária" },
+  { id: "1", name: "Conta Poupança" },
+  { id: "2", name: "Conta Salário" },
+  { id: "3", name: "Conta Corrente" },
+];
